b6eccea1e736673c17a94279f9eaca8f78388731
Soro08 <soro4827@gmail.com>
2019-11-28 17:10:16 +0000

    Initial commit

---
+++ editors/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+
+from bs4 import BeautifulSoup
+
+from cssjson import toJSON, toCSS
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'index.html')
+
+
+def compilehtml(request):
+
+    html_doc = request.POST.get('code')
+    css_doc = request.POST.get('css')
+
+    codecss = toJSON(css_doc)
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    try:
+        h = soup.find('h1')
+        h = h.text
+    except:
+        h = "h"
+
+    try:
+        p = soup.find('p', attrs={'class': 'nan'})
+        p = p.text
+
+    except:
+        p = "p"
+
+    data = {
+        'status':True,
+        'soup':h,
+        "p":p,
+        "css":codecss
+    }
+
+    return JsonResponse(data)
